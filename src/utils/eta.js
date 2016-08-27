525939f47a5ce07f436c7e9036405cbed88618be
Nathan Smith <natesmith.copper@gmail.com>
2016-08-27 14:33:11 -0400

    work

--- src/utils/eta.js
+++ src/utils/eta.js
@@ -1,19 +1,23 @@
-const dist = 5;
-let toEndShuttle = true;
-let toEndUser = true;
-let shuttlePos = 0;
-let userPos = 3;
+const eta = function(routeDist, toEndShuttle, toEndUser, shuttlePos, userPos){
 
-// same dir case
-if (toEndShuttle === toEndUser){
-    console.log('same dir')
-    if (toEndShuttle && shuttlePos < userPos){
-        console.log('shuttle behind, see you shortly')
+  // same dir case
+  if (toEndShuttle === toEndUser){
+      console.log('same dir')
+      // going toward end
+      if (toEndShuttle && shuttlePos < userPos){
+          console.log('shuttle behind, see you shortly')
+          console.log('up:', userPos)
+
+        // going toward start
+      } else{
+          console.log('shuttle ahead, I have to turn around twice')
+      }
+      // opp dir case
     } else{
-        console.log('shuttle ahead, I have to turn around twice')
+        console.log('opp dir')
+        console.log('catch you after I turn around once')
     }
-    // opp dir case
-  } else{
-      console.log('opp dir')
-      console.log('catch you after I turn around once')
-  }
+
+}
+
+eta(5, true, true, 2, 4);
