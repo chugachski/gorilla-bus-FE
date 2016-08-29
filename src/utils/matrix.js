2b5913561b6bc0c2aaa54ddb8a4da95ad1f7e366
Yilin Peng <elenayyyp@gmail.com>
2016-08-29 10:44:10 -0400

    axios to be

--- src/utils/matrix.js
+++ src/utils/matrix.js
@@ -4,22 +4,26 @@ const Matrix = {
 
   getTime: function(data) {
     // console.log('data', data)
-    const endpoint = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=';
-    const key = 'AIzaSyD5ZfvxOIfkef7YwfrAOWxniSur8U9Lwzo';
-    const fullUrl = endpoint + `${data.props.lat},${data.props.lng}&destinations=${data.props.stopLat},${data.props.stopLon}&key=${key}`;
+    const method = 'post';
+    const url = 'http://localhost:3000/distance';
+    const dataObj = {lat: data.props.lat, lng: data.props.lng, stopLat: data.props.stopLat, stopLng: data.props.stopLon};
+
+    // const endpoint = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=';
+    // const key = 'AIzaSyD5ZfvxOIfkef7YwfrAOWxniSur8U9Lwzo';
+    // const fullUrl = endpoint + `${data.props.lat},${data.props.lng}&destinations=${data.props.stopLat},${data.props.stopLon}&key=${key}`;
     // console.log('fullurl:', fullUrl);
 
     return axios({
-      method: 'GET',
-      url: fullUrl
+      method: 'POST',
+      url: url,
+      data: dataObj
     }).then(function(res){
+      console.log(res.data);
       // console.log('time resp:', res)
-      console.log('time / dist:', res.data.rows[0].elements[0].duration.value, res.data.rows[0].elements[0].distance.text)
+      // console.log('time / dist:', res.data.rows[0].elements[0].duration.value, res.data.rows[0].elements[0].distance.text)
     });
   }
 
 }
 
 export default Matrix
-
-
